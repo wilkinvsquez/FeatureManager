7929aa877279861ef27be798ecdb2983ed70ae22
wilkinvsquez <wilkinvsquez@gmail.com>
2026-01-02 18:22:37 -0600

    fix: rendering

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths for easy deployment on GitHub Pages
+  base: 'featuremanager', // Use relative paths for easy deployment on GitHub Pages
   plugins: [vue()],
 })
