d58127bf8700fd36c6e7cd79345e5ee93610517c
wilkinvsquez <wilkinvsquez@gmail.com>
2026-01-02 18:04:24 -0600

    chore: setup final deployment with gh-actions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL || '', 'http://localhost:5173', 'https://wilkinvsquez.github.io'],
     credentials: true,
   });
   app.setGlobalPrefix('api');
